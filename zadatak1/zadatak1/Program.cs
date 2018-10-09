bda60dbcedc01e15a57e33154f07634468bb70f9
Nikolina <nikolina.cesic@hotmail.com>
2018-10-09 15:43:01 +0200

    First push

---
+++ zadatak1/zadatak1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace zadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
